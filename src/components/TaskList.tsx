5848a9939dacf74611219b286e8c26e6768e1dc4
Anwar Hosen <anwarhossene@gmail.com>
2026-05-17 01:53:08 +0600

    Enhance TaskList component: add empty state message and improve layout for better user experience

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,3 +1,4 @@
+import { ClipboardList } from "lucide-react";
 import { TaskItem } from "./TaskItem";
 import type { TaskListProps } from "@/types/taskList.types";
 
@@ -7,12 +8,11 @@ export function TaskList({
   onDelete,
   onEdit,
 }: TaskListProps) {
-  return (
+  return tasks.length > 0 ? (
     // Task List Container
     <div className="flex-1 overflow-y-auto px-3 py-2 space-y-2">
       {/* Task Items */}
       {tasks.map((t) => (
-        // Task Item
         <TaskItem
           key={t.id}
           task={t}
@@ -21,6 +21,15 @@ export function TaskList({
           onEdit={onEdit}
         />
       ))}
+    </div>
+  ) : (
+    // Empty list
+    <div className="text-muted flex flex-1 flex-col items-center justify-center gap-1 -mt-40 pointer-events-none">
+      <ClipboardList size={32} />
+      <div className="text-sm font-semibold">No task yet?</div>
+      <div className="max-w-xs text-xs leading-relaxed">
+        Click the + icon below to get started.
+      </div>
     </div>
   );
 }
