c2f14d6b364553a4d47087e02d921ae81e0b6ed6
Anwar Hosen <anwarhossene@gmail.com>
2026-05-16 20:46:35 +0600

    Add constants for app name, version, GitHub profile, repository, and license

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = "Tasks";
+export const APP_VERSION = "1.0.1";
+
+export const GITHUB_PROFILE = "https://github.com/AnwarHosenDev";
+export const GITHUB_REPO = "https://github.com/AnwarHosenDev/tasks";
+export const LICENCE =
+  "https://github.com/AnwarHosenDev/tasks/blob/main/Licence";
