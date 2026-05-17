7a952f32d80fa215569473c5ae46dbf08f9e0e9d
Anwar Hosen <anwarhossene@gmail.com>
2026-05-18 04:24:40 +0600

    Add download link for mobile app and update GitHub repository link

--- src/components/AppDrawer.tsx
+++ src/components/AppDrawer.tsx
@@ -1,7 +1,13 @@
-import { X, Moon, Sun, BookOpen, GitBranch } from "lucide-react";
+import { X, Moon, Sun, BookOpen, GitBranch, Download } from "lucide-react";
 import { Drawer } from "@/components/Drawer";
 import type { AppDrawerProps, SidebarItem } from "@/types/appDrawer.types";
-import { APP_VERSION, GITHUB_PROFILE, GITHUB_REPO, LICENCE } from "@/constants";
+import {
+  APP_VERSION,
+  GITHUB_PROFILE,
+  GITHUB_REPO,
+  LICENCE,
+  GITHUB_RELEASES,
+} from "@/constants";
 
 export function AppDrawer({
   open,
@@ -33,7 +39,7 @@ export function AppDrawer({
         </div>
 
         {/* Body */}
-        <div className="flex-1 p-6 space-y-8">
+        <div className="flex-1 p-6 space-y-6">
           {/* Sidebar items */}
           {sidebarItem.map((item, index) => {
             return (
@@ -41,6 +47,7 @@ export function AppDrawer({
                 key={index}
                 href={item.link}
                 target="_blank"
+                rel="noreferrer noopener"
                 className="flex items-center gap-2 text-[16px] text-secondary hover:underline"
               >
                 <item.icon size={14} />
@@ -64,6 +71,7 @@ export function AppDrawer({
           <a
             href={GITHUB_REPO}
             target="_blank"
+            rel="noreferrer noopener"
             className="flex items-center gap-2 text-xs text-secondary hover:underline"
           >
             <GitBranch size={14} />
@@ -72,6 +80,22 @@ export function AppDrawer({
 
           <span>Version {APP_VERSION}</span>
         </div>
+
+        <a
+          href={GITHUB_RELEASES}
+          target="_blank"
+          rel="noreferrer noopener"
+          className="
+            inline-flex items-center justify-center gap-2 
+            rounded-xl border border-border bg-surface 
+            px-4 py-3 text-xs font-medium text-secondary 
+            transition hover:bg-border/10 sm:w-auto mx-4 mb-4
+          "
+        >
+          <Download size={14} />
+          <span className="hidden sm:inline">Download app</span>
+          <span className="sm:hidden">Latest version</span>
+        </a>
       </div>
     </Drawer>
   );
--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,9 @@ export const APP_NAME = "Tasks";
 export const APP_VERSION = "1.0.3";
 
 export const GITHUB_PROFILE = "https://github.com/AnwarHosenDev";
-export const GITHUB_REPO = "https://github.com/AnwarHosenDev/tasks";
+export const GITHUB_REPO =
+  "https://github.com/AnwarHosenDev/tasks/tree/android";
+export const GITHUB_RELEASES =
+  "https://github.com/AnwarHosenDev/tasks/releases/latest";
 export const LICENCE =
   "https://github.com/AnwarHosenDev/tasks/blob/main/Licence";
